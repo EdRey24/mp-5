9a5b36ed48b7eb2806e6adf8600ca1375c027683
Edward Reyna <reynaedward108@gmail.com>
2026-04-03 15:23:11 -0400

    Implemented FinishedUrl Component but missing styling

--- app/components/FinishedUrl.tsx
+++ app/components/FinishedUrl.tsx
@@ -1,7 +1,25 @@
-export default function FinishedUrl(){
+import {useState} from "react";
+
+export default function FinishedUrl({shortenedUrl}: {shortenedUrl: string}){
+    const [copied, setCopied] = useState<boolean>(false);
+
+    const handleCopy = async (urlToCopy: string) => {
+        try {
+            await navigator.clipboard.writeText(urlToCopy);
+            setCopied(true);
+            setTimeout(() => setCopied(false), 2000);
+        } catch (err) {
+            console.error("Failed to copy!", err);
+        }
+    };
+
     return (
-        <>
+        <div>
             <p>Your shortened URL:</p>
-        </>
+            <a href={shortenedUrl} target="_blank">{shortenedUrl}</a>
+            <button onClick={() => handleCopy(shortenedUrl)}>
+                {copied ? "Copied!" : "Copy"}
+            </button>
+        </div>
     );
 }
